cc90a9405353d84b2cbc6c6fa51430dae5b1435c
parkwww <park.www@lookitsuni.com>
2015-02-17 11:52:17 -0800

    Using variables like an adult.

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+GIT_USERNAME="parkwww"
+GIT_EMAIL="park.www@lookitsuni.com"
+
 if [ "$TRAVIS_BRANCH" = "dev" ]; then
   if command -v git >/dev/null 2>&1 && command -v npm >/dev/null 2>&1; then
-    git config --local user.name "parkwww"
-    git config --local user.email "park.www@lookitsuni.com"
+    git config --local user.name ${GIT_USERNAME}
+    git config --local user.email ${GIT_EMAIL}
     git config --local push.default matching
     npm version patch -m $'v%s\n\n[skip ci]' &&
     git push https://${GH_OAUTH_TOKEN}@github.com/helior/foxtail HEAD:dev --tag
