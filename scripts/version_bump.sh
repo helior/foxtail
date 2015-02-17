7233de2fa21361cad35b0bba713014cb8e7a3ef5
helior <me@helior.info>
2015-02-17 12:13:33 -0800

    Checking for Github oauth token..

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -3,14 +3,13 @@
 GIT_USERNAME="parkwww"
 GIT_EMAIL="park.www@lookitsuni.com"
 
-if [ "$TRAVIS_BRANCH" = "dev" ]; then
+if [ "$TRAVIS_BRANCH" = "dev" ] && [ ${GH_OAUTH_TOKEN:?"Github OAuth token must be set."} ]; then
   if command -v git >/dev/null 2>&1 && command -v npm >/dev/null 2>&1; then
     git config --local user.name ${GIT_USERNAME}
     git config --local user.email ${GIT_EMAIL}
     git config --local push.default matching
     npm version patch -m $'v%s\n\n[skip ci]' &&
     git push https://${GH_OAUTH_TOKEN}@github.com/helior/foxtail HEAD:dev --tag
-    echo "good."
   else
     echo "Failed. Git and NPM are required for version bumping..";
   fi
