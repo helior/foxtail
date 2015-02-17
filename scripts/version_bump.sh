6504d798e6e3c55f2fa0c0b144a4c6d3afcc9cbe
parkwww <park.www@lookitsuni.com>
2015-02-17 11:50:52 -0800

    Fix syntax error.

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = "dev" ]; then
-  if command -v git 2>/dev/null && command -v npm 2>/dev/null; then
+  if command -v git >/dev/null 2>&1 && command -v npm >/dev/null 2>&1; then
     git config --local user.name "parkwww"
     git config --local user.email "park.www@lookitsuni.com"
     git config --local push.default matching
-    npm version patch -m $'v%s\n\n[skip ci]'
+    npm version patch -m $'v%s\n\n[skip ci]' &&
     git push https://${GH_OAUTH_TOKEN}@github.com/helior/foxtail HEAD:dev --tag
+    echo "good."
   else
     echo "Failed. Git and NPM are required for version bumping..";
   fi
+fi
