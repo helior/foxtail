4da18a2a8b644f08c2af2a0d93b3fdb45156da58
helior <me@helior.info>
2015-03-13 13:48:27 -0700

    Cleaning house

--- routes/testRoute.jsx
+++ routes/testRoute.jsx
@@ -2,25 +2,26 @@
 
 var React = require('react');
 var Reflux = require('reflux');
-var Action = require('../actions/testAction');
+// var Action = require('../actions/testAction');
 var Store = require('../stores/testStore');
+
+var Jumbotron = require('react-bootstrap').Jumbotron;
 
 var TestRoute = React.createClass({
   mixins: [ Reflux.connect(Store, 'list') ],
 
   statics: {
     willTransitionTo: function(transition, params, query, callback) {
-      Action.load();
+      // Action.load();
       callback();
     }
   },
 
   render() {
-    return <ul>
-      {this.state.list.map((item, index) =>
-        <li key={index}>{item.answer}</li>
-      )}
-    </ul>
+    return <Jumbotron>
+      <h1>Simmer down now</h1>
+      <p>This is just a test page.</p>
+    </Jumbotron>
   }
 });
 
