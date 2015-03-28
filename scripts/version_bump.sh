77df2e6c775472b7929d6629538c064d9f0f6b33
helior <me@helior.info>
2015-03-28 12:46:24 -0700

    Testing Travis

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-GIT_USERNAME="parkwww"
-GIT_EMAIL="park.www@lookitsuni.com"
+GIT_USERNAME="helior"
+GIT_EMAIL="me@helior.info"
 
+ENV
 if [ "$TRAVIS_BRANCH" = "dev" ] && [ ${GH_OAUTH_TOKEN:?"Github OAuth token must be set."} ]; then
   if command -v git >/dev/null 2>&1 && command -v npm >/dev/null 2>&1; then
     git config --local user.name ${GIT_USERNAME}
     git config --local user.email ${GIT_EMAIL}
     git config --local push.default matching
     npm version patch -m $'v%s\n\n[skip ci]' &&
-    git push https://${GH_OAUTH_TOKEN}@github.com/helior/foxtail HEAD:dev --tag
+    git push https://${GH_OAUTH_TOKEN}@github.com/vulpnea/foxtail HEAD:dev --tag
   else
     echo "Failed. Git and NPM are required for version bumping..";
   fi
